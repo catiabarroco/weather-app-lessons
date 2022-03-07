5e19ec3f1affd04b0ef27c64c7353c4264ca85aa
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2022-03-07 16:04:32 +0000

    added  he GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://github.com/catiabarroco">Cátia Barroco</a> and is
+        <a
+          href="https://github.com/catiabarroco/weather-app-lessons"
+          target="_blank"
+        >
+          {" "}
+          open-soureced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

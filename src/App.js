f54530ed78925b7e492d166e4abe49890ca4c96d
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2022-03-07 16:33:09 +0000

    added bootstarp

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        {" "}
-        This project was coded by{" "}
-        <a href="https://github.com/catiabarroco" target="_blank" rel="noreferrer">Cátia Barroco</a> and is
-        <a
-          href="https://github.com/catiabarroco/weather-app-lessons"
-          target="_blank" rel="noreferrer"
-        >
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
           {" "}
-          open-soureced on GitHub
-        </a>
-      </footer>
+          This project was coded by{" "}
+          <a href="https://github.com/catiabarroco" target="_blank" rel="noreferrer">Cátia Barroco</a> and is
+          <a
+            href="https://github.com/catiabarroco/weather-app-lessons"
+            target="_blank" rel="noreferrer"
+          >
+            {" "}
+            open-soureced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

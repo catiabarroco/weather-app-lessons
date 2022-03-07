df5b6509d148d3682f6c4e9e84a8d6e3fdb8beeb
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2022-03-07 17:04:05 +0000

    add weather component

---
+++ src/Weather.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+          <div className="col-6">
+              
+              </div>
+
+          </div>
+    </div>
+  );
+}
